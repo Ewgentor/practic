e9c93772224c12b4aacac53a878aa30985d4f058
ewgen <ewgentor@gmail.com>
2024-06-23 23:50:29 +0300

    404 handler

--- flsite.py
+++ flsite.py
@@ -18,5 +18,10 @@ def news():
     return render_template('news.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(debug=True)
