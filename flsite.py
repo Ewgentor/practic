fec9269a45f12099926939fd7caa20595c011ff1
ewgen <ewgentor@gmail.com>
2024-06-25 10:48:53 +0300

    search implemented

--- flsite.py
+++ flsite.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for
 import json
 
 
 app = Flask(__name__)
+
+
+with open('static/db/products.json', encoding='utf-8') as f:
+    data = json.load(f)['products']
 
 
 @app.route("/")
@@ -12,8 +16,6 @@ def index():
 
 @app.route("/catalog")
 def catalog():
-    with open('static/db/products.json', encoding='utf-8') as f:
-        data = json.load(f)['products']
     return render_template('catalog.html', data=data)
 
 
@@ -25,6 +27,16 @@ def news():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@app.route('/search',methods=['POST'])
+def search_results():
+    query = request.form['query']
+    result = []
+    for el in data:
+        if query.lower() in data[el]['name'].lower():
+            result.append(data[el])
+    return render_template('search_results.html', result=result)
 
 
 if __name__ == "__main__":
